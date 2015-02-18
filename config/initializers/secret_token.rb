3b9b80196b4f727140ddb2e31c6bf1bb726a4ec2
Zach Brown <brownzach125@gmail.com>
2015-02-17 21:02:31 -0600

    Initial Project Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bingo::Application.config.secret_key_base = '9420cc6089c0ea850f2fa10c5c3ac0e074e040ae0d5a63836b343d57569e654cdcc589f7eff9c186169564d98ac9c50814a02bde445fe2119d9df8d5cc77a8e7'
